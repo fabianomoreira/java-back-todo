c52ba3ba1f40b19493a770061ef86dd12f34cf29
Fabiano <professor@fabianomoreira.com.br>
2023-09-27 21:03:50 -0300

    Configuração para CORS

---
+++ backtodo/src/main/java/resource/CORSFilter.java
@@ -0,0 +1,17 @@
+package resource;
+
+import java.io.IOException;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+
+public class CORSFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+    	responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+    	responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+    	responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+    	responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
--- backtodo/src/main/java/resource/TarefaResource.java
+++ backtodo/src/main/java/resource/TarefaResource.java
@@ -2,8 +2,13 @@ package resource;
 
 import java.util.List;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -12,6 +17,7 @@ import service.TarefaService;
 
 @Path("tarefa")
 public class TarefaResource {
+	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("listar")
@@ -23,4 +29,78 @@ public class TarefaResource {
 		
 		return response;
 	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("tarefas/{id}")
+	public Response getTarefaPorId(@PathParam("id") Integer id) {
+		TarefaService service = new TarefaService();
+		Tarefa tarefa = service.buscarTarefaPorId(id);
+		
+		Response response =  null;
+		
+		if(tarefa != null) {
+			response = Response.status(Response.Status.OK).entity(tarefa).build();
+		} else {
+			response = Response.status(Response.Status.NOT_FOUND).entity("A busca não retornou resultados válidos").build();
+		}
+		
+		return response;
+	}
+	
+	@DELETE
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("tarefas/{id}")
+	public Response setDeletarTarefa(@PathParam("id") Integer id) {
+		TarefaService service = new TarefaService();
+		boolean retorno = service.excluirTarefa(id);
+		
+		Response response = null;
+		
+		if(retorno) {
+			response = Response.status(Response.Status.OK).build();
+		} else {
+			response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Não foi possível realizar a exclusão").build();
+		}
+		
+		return response;
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response setPostTarefa(Tarefa tarefa) {
+		TarefaService service = new TarefaService();
+		boolean retorno = service.incluirTarefa(tarefa);
+		
+		Response response = null;
+		
+		if(retorno) {
+			response = Response.status(Response.Status.CREATED).entity(tarefa).build();
+		} else {
+			response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Não foi possível realizar a inclusão").build();
+		}
+		
+		return response;
+	}
+	
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response setPutTarefa(Tarefa tarefa) {
+		TarefaService service = new TarefaService();
+		boolean retorno = service.alterarTarefa(tarefa);
+		
+		Response response = null;
+		
+		if(retorno) {
+			response = Response.status(Response.Status.OK).entity(tarefa).build();
+		} else {
+			response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Não foi possível realizar a alteração").build();
+		}
+		
+		return response;
+	}	
 }
